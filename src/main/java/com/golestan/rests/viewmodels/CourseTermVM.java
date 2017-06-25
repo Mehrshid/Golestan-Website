9006b3c9d6c1232a379abda53eadeb6b104f70a6
Yasamanro <yasamanro@Yasamans-MBP.Home>
2017-06-25 12:35:15 +0430

    4 View Models Added
    1. ChangePasswordVM
    2. CourseTermVM
    3. CourseVM
    4. LoginVM

---
+++ src/main/java/com/golestan/rests/viewmodels/CourseTermVM.java
@@ -0,0 +1,29 @@
+package com.golestan.rests.viewmodels;
+
+
+public class CourseTermVM {
+
+	public CourseVM courseVM;
+	
+	public TermVM termVM;
+
+	public CourseVM getCourseVM() {
+		return courseVM;
+	}
+
+	public void setCourseVM(CourseVM courseVM) {
+		this.courseVM = courseVM;
+	}
+
+	public TermVM getTermVM() {
+		return termVM;
+	}
+
+	public void setTermVM(TermVM termVM) {
+		this.termVM = termVM;
+	}
+
+
+	
+	
+}
